932bb341bd0d1152ffde3b349e5ce81345f9949c
Leocruz721 <leojancruz10@gmail.com>
2025-02-18 14:52:00 -0700

    Add additional alert messages and improve output in main.js

--- Basic-JavaScript-Projects/Project1_expressions_alerts/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alerts/JS/main.js
@@ -2,4 +2,8 @@ window.alert("Hello, World!");
 document.write("Hello, World!");
 var A = "Hello, Welcome to Everyone";
 document.write(A);
+var message = "Hello, Welcome!";
+window.alert(message);
+var message = "He said, \"It's a beautiful day!\"";
+window.alert(message);
 
