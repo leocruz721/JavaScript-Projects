4354246f0c165a36b80c2032c6c744223c4e3e59
Leocruz721 <leojancruz10@gmail.com>
2025-02-19 14:32:13 -0700

    Enhance myFunction with comments for clarity and update changeText function in main.js

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -10,9 +10,9 @@ function changeText() {
     // Use document.getElementById to change the text of the paragraph with id "output"
     document.getElementById("output").innerHTML = message;
 }
-
+//Function that uses the += operator
 function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById('Concatenate').innerHTML = sentence;
+    var sentence = "I am learning"; //Creating the variable
+    sentence += " a lot from this book!"; //using the += operator to add to the variable
+    document.getElementById('Concatenate').innerHTML = sentence; //using document.getElementByID()
 }
