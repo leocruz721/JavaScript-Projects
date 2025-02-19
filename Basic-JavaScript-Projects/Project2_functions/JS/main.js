4ce768188ffbf9b4d099323a8990b5c942637591
Leocruz721 <leojancruz10@gmail.com>
2025-02-18 18:00:10 -0700

    Add changeText function and update index.html for text modification

---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+// Function that assigns two variables and modifies an HTML element using document.getElementById
+function changeText() {
+    // Assigning values to two variables
+    var greeting = "Hello, ";
+    var name = "Alice!";
+
+    // Concatenate the two variables
+    var message = greeting + name;
+
+    // Use document.getElementById to change the text of the paragraph with id "output"
+    document.getElementById("output").innerHTML = message;
+}
