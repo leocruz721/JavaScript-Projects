cd9af81dbcf07086a7a57b5c5fbf10baa9072a1e
Leocruz721 <leojancruz10@gmail.com>
2025-02-20 13:21:35 -0700

    Refactor performMath function for improved clarity and add additional mathematical operations; update index.html to enhance user interaction

--- Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
+++ Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
@@ -1,20 +1,29 @@
-// JS/main.js
-
-// JavaScript function that performs multiple mathematical operations and updates the paragraph element
 function performMath() {
-    const additionResult = 5 + 3;          // Addition operation
-    const subtractionResult = 5 - 3;       // Subtraction operation
-    const multiplicationResult = 5 * 3;    // Multiplication operation
-    const divisionResult = 5 / 3;          // Division operation (will result in a decimal)
-    const modulusResult = 5 % 3;           // Modulus operation (remainder of division)
-    const exponentiationResult = 5 ** 3;   // Exponentiation operation (5 raised to the power of 3)
+    var addition = 2 + 2;  // Addition operation
+    var subtraction = 5 - 3;  // Subtraction operation
+    var multiplication = 6 * 3;  // Multiplication operation
+    var division = 12 / 4;  // Division operation
+    var simple_Math = (1 + 2) * 10 / 2 - 5;  // Multiple Operations
+    var modulus = 7 % 3; // Modulus operation
 
-    // Update the content of the paragraph with the results of all operations
-    document.getElementById("Math").innerText = 
-        "The result of 5 + 3 is: " + additionResult + "\n" +
-        "The result of 5 - 3 is: " + subtractionResult + "\n" +
-        "The result of 5 * 3 is: " + multiplicationResult + "\n" +
-        "The result of 5 / 3 is: " + divisionResult.toFixed(2) + "\n" + // Format division result to 2 decimal places
-        "The result of 5 % 3 is: " + modulusResult + "\n" +
-        "The result of 5 ** 3 is: " + exponentiationResult;
+    var negationTrue = !true;  // Negation of true
+    var negationFalse = !false;  // Negation of false
+    var negationZero = !0;  // Negation of 0
+    var negationOne = !1;  // Negation of 1
+
+    // Display the results in the paragraph with id "Math"
+    document.getElementById("Math").innerHTML = 
+        "2 + 2 = " + addition + "<br>" +  // Addition result
+        "5 - 3 = " + subtraction + "<br>" +  // Subtraction result
+        "6 * 3 = " + multiplication + "<br>" +  // Multiplication result
+        "12 / 4 = " + division + "<br>" +  // Division result
+        "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals: " + simple_Math + "<br>" +  // Multiple Result
+        "7 % 3 = " + modulus+ "<br>" +  // Modulus Result
+        "!true = " + negationTrue + "<br>" +  // Negation of true
+        "!false = " + negationFalse + "<br>" +  // Negation of false
+        "!0 = " + negationZero + "<br>" +  // Negation of 0
+        "!1 = " + negationOne;  // Negation of 1
 }
+
+
+
