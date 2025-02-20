1469983830a2576891fbd47be01e968b5c1c9eec
Leocruz721 <leojancruz10@gmail.com>
2025-02-20 11:11:33 -0700

    Enhance performMath function to include multiplication and division operations with updated output formatting

--- Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
+++ Basic-JavaScript-Projects/Project3_math_operators/JS/main.js
@@ -1,9 +1,13 @@
-// JavaScript function that performs both addition and subtraction and updates the paragraph element
+// JavaScript function that performs addition, subtraction, multiplication, and division and updates the paragraph element
 function performMath() {
     const additionResult = 5 + 3; // Addition operation
     const subtractionResult = 5 - 3; // Subtraction operation
+    const multiplicationResult = 5 * 3; // Multiplication operation
+    const divisionResult = 5 / 3; // Division operation (will result in a decimal)
 
     // Update the content of the paragraph with the results
     document.getElementById("Math").innerText = "The result of 5 + 3 is: " + additionResult + "\n" +
-                                                 "The result of 5 - 3 is: " + subtractionResult;
+                                                 "The result of 5 - 3 is: " + subtractionResult + "\n" +
+                                                 "The result of 5 * 3 is: " + multiplicationResult + "\n" +
+                                                 "The result of 5 / 3 is: " + divisionResult.toFixed(2); // Format division result to 2 decimal places
 }
