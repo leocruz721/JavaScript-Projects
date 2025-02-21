c0cdbafe47ccda0b1c62414ffbd0a08a35cf835f
Leocruz721 <leojancruz10@gmail.com>
2025-02-21 12:02:30 -0700

    Add voting age check functionality using ternary operator in main.js

--- Basic-JavaScript-Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic-JavaScript-Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -4,3 +4,14 @@ function Ride_Function (){
     Can_ride = (Height < 52) ? "You are too short":"you are all enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function checkVotingAge() {
+    // Get the user's age from the input field
+    let age = document.getElementById("age").value;
+
+    // Use the ternary operator to determine if the user can vote
+    let message = (age >= 18) ? "You can vote!" : "You are not old enough to vote.";
+
+    // Display the message in the paragraph with id "result"
+    document.getElementById("result").innerText = message;
+}
