fce940787ba32882f4dee6f4ab4ff95c644d933d
Leocruz721 <leojancruz10@gmail.com>
2025-02-24 16:56:34 -0700

    Add text change functionality with button in HTML and update async.js

--- Basic-JavaScript-Projects/Async_assignment/JS/async.js
+++ Basic-JavaScript-Projects/Async_assignment/JS/async.js
@@ -1,1 +1,6 @@
 alert.apply(document.getElementById("p1").innerHTML)
+
+function changeText() {
+    // Reference an HTML element with the ID 'myElement' and change its text content
+    document.getElementById("myElement").textContent = "The text has been changed!";
+}
