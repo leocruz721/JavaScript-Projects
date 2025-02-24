f0f8fc79e4d7b13518ff0135640a95c9a8ac5c80
Leocruz721 <leojancruz10@gmail.com>
2025-02-24 16:26:41 -0700

    Update string and sentence methods with new examples for demonstration

--- Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
+++ Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
@@ -1,25 +1,25 @@
 function full_Sentence() {
-    var part_1 = "I have ";
-    var part_2 = "made this ";
-    var part_3 = "into a complete ";
-    var part_4 = "sentence. ";
+    var part_1 = "Let's look ";
+    var part_2 = "at this ";
+    var part_3 = "beautiful car ";
+    var part_4 = "on the street. ";
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 
 function slice_Method() {
-    var Sentence = "All work and no play makes Johnny a dull boy.";
-    var Section = Sentence.slice(27, 33);
+    var Sentence = "Work Hard,Play Hard";
+    var Section = Sentence.slice(5, 9);
     document.getElementById("Slice").innerHTML = Section;
 }
 
 function string_Method() {
-    var X = 182;
+    var X = 985;
     document.getElementById("Numbers_to_string").innerHTML = X.toString();
 }
 
 function precision_Method() {
-    var X = 12938.3012987376112;
+    var X = 9585.4781324567;
     document.getElementById("Precision").innerHTML = X.toPrecision(10);
 }
 
