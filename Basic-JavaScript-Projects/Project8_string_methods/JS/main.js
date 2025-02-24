441bb0689c3afb98a51442092a621f77f7dc26b6
Leocruz721 <leojancruz10@gmail.com>
2025-02-24 16:16:35 -0700

    Add slice_Method function and update HTML with button and display area for sliced text

--- Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
+++ Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
@@ -6,3 +6,9 @@ function full_Sentence() {
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
+
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27, 33);
+    document.getElementById("Slice").innerHTML = Section;
+}
