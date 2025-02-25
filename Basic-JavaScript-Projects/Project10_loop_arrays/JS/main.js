300f3b73139fe0f5fdb914b370f62e673dc1babb
Leocruz721 <leojancruz10@gmail.com>
2025-02-25 14:26:10 -0700

    Add for loop functionality to display a list of instruments in HTML

---
+++ Basic-JavaScript-Projects/Project10_loop_arrays/JS/main.js
@@ -0,0 +1,12 @@
+function for_Loop() {
+    const instruments = ["Guitar", "Piano", "Drums", "Violin", "Flute"]; // Array of instruments
+    let result = ""; // Variable to store the list of instruments
+
+    // Use a for loop to iterate through the array
+    for (let i = 0; i < instruments.length; i++) {
+        result += instruments[i] + "<br>"; // Add each instrument to the result string with a line break
+    }
+
+    // Display the result in the <p> tag with id "List_of_Instruments"
+    document.getElementById("List_of_Instruments").innerHTML = result;
+}
