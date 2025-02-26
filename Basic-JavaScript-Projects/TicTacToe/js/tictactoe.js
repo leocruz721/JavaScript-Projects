f7aa99841375acfb96bc37fddc84d1a8b6201861
Leocruz721 <leojancruz10@gmail.com>
2025-02-26 11:05:35 -0700

    Remove unused X image and fix typos in Tic Tac Toe game logic

--- Basic-JavaScript-Projects/TicTacToe/js/tictactoe.js
+++ Basic-JavaScript-Projects/TicTacToe/js/tictactoe.js
@@ -4,13 +4,13 @@ let activePlayer = 'X';
 let selectedSquares = [];
 
 //This function is for placing an x or o in a square
-function placeXorO(sqaureNumber) {
-    //This condition ensures a sqaure hasn't been selected already.
+function placeXorO(squareNumber) {
+    //This condition ensures a square hasn't been selected already.
     //The .some() method is used to check each element of the selectSquare array
     //to see if it contains the swaure number clicked on.
-    if (!selectedSquares.some(Element => Element.includes(sqaureNumber))) {
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
         //This variable retreives the HTML element is that was clicked.
-        let select = document.getElementById(sqaureNumber);
+        let select = document.getElementById(squareNumber);
         //This condition checks who's turn it is.
         if (activePlayer === 'X') {
             //If activePlayer is equal to 'X', the x.png is placed in HTML
@@ -21,7 +21,7 @@ function placeXorO(sqaureNumber) {
             select.style.backgroundImage = 'url("images/O.png")';
         }
         //squareNumber and activePlayer are concatenated together and added to array.
-        selectedSquares.push(sqaureNumber + activePlayer);
+        selectedSquares.push(squareNumber + activePlayer);
         //This calls a function to check for any win condition.
         checkwinConditions();
         //This condition is for changing the active player.
@@ -41,27 +41,27 @@ function placeXorO(sqaureNumber) {
             //This funtion disbales clicking for computers turn.
             disableClick();
             //This function waits 1 second before the computer places an image and enable clicks.
-            setTimeout(function() {computerTurns()}, 1000);
+            setTimeout(function() { computersTurn(); }, 1000);
         }
         //Returning true is needed for our coumpterTurns() function to work.
         return true;
     }
     //This function results in a random square being selected by the computer.
-    function computerTurns() {
+    function computersTurn() {
         //This boolean is needed for our while loop.
-        let succes = false;
+        let success = false;
         //This varibale stores a random number 0-8.
-        let pickASqaure;
+        let pickASquare;
         //This condition allows our while loop to keepo trying if a swaure hasn't been selected yet.
-        while (!succes) {
+        while (!success) {
             //A random number between 0 and 8 is selected.
-            pickASqaure = String(Math.floor(Math.random() * 9));
-            //If the random number evaluated returns true, the sqaures hasn't been selected yet.
-            if (placeXorO(pickASqaure)) {
+            pickASquare = String(Math.floor(Math.random() * 9));
+            //If the random number evaluated returns true, the squares hasn't been selected yet.
+            if (placeXorO(pickASquare)) {
                 //This lines calls the function.
-                placeXorO(pickASqaure);
+                placeXorO(pickASquare);
                 //This changes our boolean and ends the loop.
-                succes = true;
+                success = true;
             };
         }
     }
